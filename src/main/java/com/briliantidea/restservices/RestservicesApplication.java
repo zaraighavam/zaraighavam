9d54b2e85e8082c44d3d02d72056d69084af8eb2
Zara <zahraghavam5@gmail.com>
2021-09-21 21:40:30 +0430

    test

---
+++ src/main/java/com/briliantidea/restservices/RestservicesApplication.java
@@ -0,0 +1,13 @@
+package com.briliantidea.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/briliantidea/restservices/RestservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briliantidea.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
